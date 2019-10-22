7243f3005ce694380dfdfa1114c16d62ac16108d
454902499@qq.com <454902499@qq.com>
2019-10-22 21:58:47 +0800

    增加查询停车场方法。

--- src/main/java/com/stone/controller/ParkingLotController.java
+++ src/main/java/com/stone/controller/ParkingLotController.java
@@ -1,10 +1,28 @@
 package com.stone.controller;
 
+import com.stone.bean.OrderRecord;
+import com.stone.bean.ParkingLot;
+import com.stone.service.OrderRecordService;
+import com.stone.service.ParkingLotService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/parkingLot")
 public class ParkingLotController {
 
+    @Resource
+    private ParkingLotService parkingLotService;
+    /*
+     * 根据用户名查询预约记录
+     * */
+    @GetMapping("/getAllList")
+    public List<ParkingLot> getAllList() {
+        return parkingLotService.getAllList();
+    }
 }
---
+++ src/main/java/com/stone/mapper/UserBeanMapper.java
@@ -0,0 +1,8 @@
+package com.stone.mapper;
+
+
+import com.stone.base.BaseMapper;
+import com.stone.bean.UserBean;
+
+public interface UserBeanMapper extends BaseMapper<UserBean> {
+}
